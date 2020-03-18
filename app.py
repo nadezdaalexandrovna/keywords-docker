e5b5a6d880c2adb1ca0498a4aaaad11955db872f
Nadia Okinina <nokinina@eurac.edu>
2020-03-18 08:51:09 +0100

    added json encoding

--- app.py
+++ app.py
@@ -59,7 +59,8 @@ class AskKeywords(Resource):
 
             #Make the temporary directory
             make_output_directory(outputDirectory)
-            st_u8 = st.encode('utf8')
+            #st_u8 = st.encode('utf8') # for text
+            st_u8 = json.dumps( st, ensure_ascii=False).encode('utf8') # for json
             #key_word_extractor = KeywordExtractor(st_u8, outputDirectory)
             key_word_extractor = KeywordExtractor("json", st_u8, outputDirectory)
             key_words_set = key_word_extractor.extract_keywords()
